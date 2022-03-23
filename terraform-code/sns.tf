5605d82898c259939c37c556865d958546fcf3a4
kari venkatesh <99434235+kari-venkatesh@users.noreply.github.com>
2022-03-23 12:08:32 +0530

    Update sns.tf

--- terraform-code/sns.tf
+++ terraform-code/sns.tf
@@ -7,27 +7,4 @@ resource "aws_sns_topic" "mytopic" {
   name = "mytopic-name"
 }
 
-resource "aws_sns_topic_subscription" "email-target" {
-  topic_arn = aws_sns_topic.mytopic.arn
-  protocol  = "email"
-  endpoint  = "venkyroyal66@gmail.com"
-}
-#scripct for 5xx for dev-control-plane-api
-resource "aws_cloudwatch_metric_alarm" "gateway5xx_error_1" {
-  alarm_name          = "dev-dev-control-plane-api-5xx-tf"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  alarm_description   = "5xx for dev-control-plane-api"
-  treat_missing_data  = "notBreaching"
-  metric_name         = "5XXError"
-  namespace           = "AWS/ApiGateway"
-  period              = 300
-  evaluation_periods  = 1
-  threshold           = 5
-  statistic           = "Average"
-  unit                = "Percent"
-  alarm_actions       = ["aws_sns_topic.mytopic.arn"]
-  ok_actions          = ["aws_sns_topic.mytopic.arn"]
-  dimensions = {
-    ApiName = "	demo-api"
-  }
-}
+
