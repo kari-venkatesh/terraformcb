fe1c98ff9d152b52262f9c3367fa2fb7544e7aa3
kari venkatesh <99434235+kari-venkatesh@users.noreply.github.com>
2022-03-25 08:49:17 +0530

    Create sns1.tf

---
+++ sns1/sns1.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA2GV7V4IAPECFGWFW"
+  secret_key = "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"
+}
+resource "aws_sns_topic" "mytopic" {
+  name = "mytopic-name"
+}
