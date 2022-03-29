33c825b5c0accfdaf2ac496f23e0c5dae0aebf74
kari venkatesh <99434235+kari-venkatesh@users.noreply.github.com>
2022-03-29 16:39:27 +0530

    Update sns1.tf

--- sns1/sns1.tf
+++ sns1/sns1.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA2GV7V4IAPECFGWFW"
-  secret_key = "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"
+ # access_key = "AKIA2GV7V4IAPECFGWFW"
+ # secret_key = "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"
 }
 resource "aws_sns_topic" "mytopic" {
   name = "mytopic-name"
